597ce4c906490df36260363fdb21e91efcd462b7
towcalenieszynek <111865284+towcalenieszynek@users.noreply.github.com>
2025-11-11 20:31:30 +0000

    Complete slowa solution

--- slowa/slowa.py
+++ slowa/slowa.py
@@ -1,6 +1,20 @@
 with open('./slowa/slowa_przyklad.txt', 'r') as f:
     slowa = [linia.strip() for linia in f]
 
+#1 podpunkt
+def znajdz_wystapienie(slowo):
+    for i in range(len(slowo)-2):
+        if slowo[i] == 'k' and slowo[i+2] == 't':
+            return True
+    return False
+
+res = 0
+for slowo in slowa:
+    res += 1 if znajdz_wystapienie(slowo) else 0
+
+print('1 podpunkt')
+print(res)
+
 #2 podpunkt
 res = []
 for slowo in slowa:
@@ -14,3 +28,17 @@ for slowo in slowa:
 print('2 podpunkt')
 print(len(res))
 print(max(res, key=len))
+
+#3 podpunkt
+print('3 podpunkt')
+for slowo in slowa:
+    wystapienia = {}
+    for litera in slowo:
+        if litera not in wystapienia:
+            wystapienia[litera] = 0
+        wystapienia[litera] += 1
+    l = len(slowo)
+    for litera in wystapienia:
+        if wystapienia[litera] >= l / 2:
+            print(slowo)
+            break
