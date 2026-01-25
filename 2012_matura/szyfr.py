b635df21d3023942ae960c7a3e532ac7eb4c5fc3
towcalenieszynek <111865284+towcalenieszynek@users.noreply.github.com>
2026-01-25 21:20:30 +0000

    Complete szyfry solution

--- 2012_matura/szyfr.py
+++ 2012_matura/szyfr.py
@@ -1,15 +1,26 @@
 import math
 
-with open('tj.txt', 'r') as f:
+with open('./2012_matura/tj.txt', 'r') as f:
     slowa = [t.strip() for t in f]
 
-with open('klucze1.txt', 'r') as f:
+with open('./2012_matura/klucze1.txt', 'r') as f:
     klucze = [t.strip() for t in f]
 
 do_szyfrowania = dict()
 
 for i in range(len(slowa)):
     do_szyfrowania[slowa[i]] = klucze[i]
+
+with open('./2012_matura/sz.txt', 'r') as f:
+    slowa = [t.strip() for t in f]
+
+with open('./2012_matura/klucze2.txt', 'r') as f:
+    klucze = [t.strip() for t in f]
+
+do_deszyfrowania = dict()
+
+for i in range(len(slowa)):
+    do_deszyfrowania[slowa[i]] = klucze[i]
 
 
 #szyfrowanie - OK
@@ -19,7 +30,7 @@ def szyfruj(tekst: str, klucz: str) -> str:
     klucz = klucz_temp
     zaszyfrowany = ''
     for j in range(len(tekst)):
-        zaszyfrowany += chr(65 + (ord(tekst[j]) + ord(klucz[j]))%26 + 1)
+        zaszyfrowany += chr(65 + (ord(tekst[j]) + ord(klucz[j])+1)%26)
     return zaszyfrowany
 
 #deszyfrowanie - OK
@@ -29,6 +40,21 @@ def deszyfruj(tekst: str, klucz: str) -> str:
     klucz = klucz_temp
     deszyfrowany = ''
     for j in range(len(tekst)):
-        deszyfrowany += chr(65 + (ord(tekst[j]) - ord(klucz[j])) % 26 - 1)
+        deszyfrowany += chr(65 + (ord(tekst[j]) - ord(klucz[j]) - 1) % 26 )
     return deszyfrowany
+
+print('Dlugosc do szyfrowania:', len(do_szyfrowania))
+with open('./2012_matura/wynik4a.txt', 'w') as plik:
+    for tekst, klucz in do_szyfrowania.items():
+        print(f'{tekst} - {klucz} = {szyfruj(tekst, klucz)}')
+        plik.write(szyfruj(tekst, klucz)+'\n')
+    plik.close()
+
+print('Dlugosc do deszyfrowania:', len(do_deszyfrowania))
+with open('./2012_matura/wynik4b.txt', 'w') as plik:
+    for tekst, klucz in do_deszyfrowania.items():
+        print(f'{tekst} - {klucz} = {deszyfruj(tekst, klucz)}')
+        plik.write(deszyfruj(tekst, klucz)+'\n')
+    plik.close()
+
 
